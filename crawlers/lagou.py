952333e6c5896ff4b3b5362c8ed711c561b5182e
Anders Cui <anders.cui@ef.com>
2015-04-21 23:31:30 +0800

    download search list

--- crawlers/lagou.py
+++ crawlers/lagou.py
@@ -2,11 +2,18 @@
 import re
 
 from bs4 import BeautifulSoup
-from urllib2 import urlopen
+from urllib2 import urlopen, URLError
+import time
 from common.chinese import write, read_all
 from common.persistence import to_pickle, from_pickle
 
 BASE_URL = 'http://www.lagou.com/'
+
+skill_url = BASE_URL + 'jobs/list_{0}?kd={0}&spc=1&pl=&gj=&xl=&yx=&gx=&st=&labelWords=label&lc=&workAddress=&city' \
+                       '={1}&requestId=&pn={2}'
+skill_url = unicode(skill_url)
+
+job_detail_url = 'http://www.lagou.com/jobs/{0}.html?source=search'
 
 
 def make_search_url(urlfmt, keyword, city, pn):
@@ -22,14 +29,28 @@ def make_soup(html):
 
 
 def save_jobs_html(url, skill, pn=1):
-    html = urlopen(url.encode('utf-8')).read()
-    write(u'./html/{0}_{1}.html'.format(skill, pn), html.decode('utf-8'))
-    return html
+
+    try:
+        html = urlopen(url.encode('utf-8')).read()
+    except URLError, e:
+        if hasattr(e, 'code'):
+            print(u'http error occured for skill: {0}, code: {1}'.format(skill, e.code))
+        elif hasattr(e, 'reason'):
+            print('server not reachable: ' + e.reason)
+
+        return ''
+    else:
+        write(u'./html/{0}_{1}.html'.format(skill, pn), html.decode('utf-8'))
+        return html
 
 
 def download_search_results(skill_url_fmt, kd, city, page):
     url = make_search_url(skill_url_fmt, kd, city, page)
     html = save_jobs_html(url, kd, page)
+    time.sleep(1)
+
+    if not html:
+        return None
 
     currPage = re.search(r'currPage: (\d+)', html)
     pageCount = re.search(r'pageCount: (\d+)', html)
@@ -39,8 +60,7 @@ def download_search_results(skill_url_fmt, kd, city, page):
         pageCount = int(pageCount.group(1))
 
         if curr < pageCount:
-            download_search_results(skill_url_fmt, kd, city, curr+1)
-
+            download_search_results(skill_url_fmt, kd, city, curr + 1)
 
 
 def save_html(url, file_name):
@@ -50,12 +70,6 @@ def save_html(url, file_name):
     print(html)
     write('./html/' + file_name, html.decode('utf-8'))
 
-#
-# def save_jobs_html(url, skill, pn=1):
-#     html = urlopen(url.encode('utf-8')).read()
-#     print(html)
-#     write('./html/' + '{0}_{1}.html'.format(skill, pn), html.decode('utf-8'))
-#
 
 def category_contents(cat_tag):
     header = cat_tag.find('div', 'menu_main').find('h2').text
@@ -90,7 +104,7 @@ if __name__ == '__main__':
     # cats[i] = category_contents(cat)
     #
     # for i in cats:
-    #     cat = cats[i]
+    # cat = cats[i]
     #     print(u'** {0} **'.format(cat['name']))
     #     for sub in cat['sub_cats']:
     #         print(sub[0])
@@ -99,42 +113,38 @@ if __name__ == '__main__':
     #
     # to_pickle(cats, 'cats.pkl')
 
-    # cats = from_pickle('cats.pkl')
-    # for i in cats:
-    #     cat = cats[i]
-    #     print(u'** cat {0}: {1} **'.format(i, cat['name']))
-    #     for sub in cat['sub_cats']:
-    #         print(sub[0])
-    #         for skill, href in sub[1]:
-    #             print('\t-' + skill + ' - ' + href)
-
 
     # ## jobs of one skill
-    skill_url = BASE_URL + 'jobs/list_{0}?kd={0}&spc=1&pl=&gj=&xl=&yx=&gx=&st=&labelWords=label&lc=&workAddress=&city' \
-                           '={1}&requestId=&pn={2}'
-    skill_url = unicode(skill_url)
+
     # # skill_url = 'http://www.lagou.com/zhaopin/ziranyuyanchuli?labelWords=label'
     # spelling = 'ziranyuyanchuli'
     page = 1
     city = u'全国'
-    kd = u'自然语言处理'
-    search_url = make_search_url(skill_url, kd, city, page)
+    # kd = u'自然语言处理'
+    # search_url = make_search_url(skill_url, kd, city, page)
 
-    download_search_results(skill_url, kd, city, page)
+    # download_search_results(skill_url, kd, city, page)
+
+    cs = 'C%23'
+    skip_list = ['Java', 'Python', 'PHP', '.NET']
+    cats = from_pickle('cats.pkl')
+    for i in cats:
+        cat = cats[i]
+        print(u'** cat {0}: {1} **'.format(i, cat['name']))
+        for sub in cat['sub_cats']:
+            print(sub[0])
+
+            for skill, href in sub[1]:
+                if skill in skip_list:
+                    print('skip ' + skill)
+                    continue
+
+                print('\t-' + skill + ' - ' + href)
+                if skill == 'C#':
+                    skill = cs
+
+                # print(make_search_url(skill_url, skill, city, page))
+                download_search_results(skill_url, skill, city, page)
+
     # print(search_url)
     # save_jobs_html(search_url, spelling, page)
-
-    # # parse job list html
-    # html = read_all('./html/ziranyuyanchuli_1.html')
-    # soup = make_soup(html)
-    #
-    # m = re.search(r'currPage: (\d+)', html)
-    # print(m.group(1))
-    # curr = re.search(r"currPagef: (\d+)", html).group(1)
-    # pageCount = re.search(r"pageCounft: (\d+)", html).group(1)
-    # print(curr)
-    # print(pageCount)
-
-
-    job_detail_url_format = 'http://www.lagou.com/jobs/143205.html?source=search'
-
